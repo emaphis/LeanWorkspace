81819477b687d555c7864d030ff99208f36fc6f4
emaphis <emaphis85@gmail.com>
2025-06-04 21:57:08 -0400

    Upto section 1.2

---
+++ FunctionalProgramming/FunctionalProgramming/ch01/02_Types.lean
@@ -0,0 +1,14 @@
+-- Types
+
+-- declaring
+
+#eval (1 - 2 : Nat)  --  0
+
+#eval (1 - 2 : Int)  --  -1
+
+--  checking the type
+#check (1 - 2 : Int)  --  1 - 2 : Int
+
+--  Impossible types
+
+--  #check String.append ["HELLO", " "] "world"
