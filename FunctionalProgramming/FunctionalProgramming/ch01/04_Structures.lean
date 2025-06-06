68abea05d57b8428084d329c461e90fdf498438e
emaphis <emaphis85@gmail.com>
2025-06-06 01:01:18 -0400

    Section 1.4 - Structures.

---
+++ FunctionalProgramming/FunctionalProgramming/ch01/04_Structures.lean
@@ -0,0 +1,98 @@
+--  1.4 Structures
+
+--  Floating point type
+
+#check 1.2  --  : Float
+
+#check -454.2123215  --  : Float
+
+#check 0.0  --  : Float
+
+#check 0  -- : Nat
+
+#check (0 : Float) --  : Float
+
+
+--  A Cartesian point
+
+structure Point where
+  x : Float
+  y : Float
+deriving Repr
+
+def origin : Point := { x := 0.0, y := 0.0 }
+
+#eval origin
+
+#eval origin.x  -- 0.000000
+#eval origin.y
+
+def addPoints (p1 : Point) (p2 : Point) : Point :=
+  { x := p1.x + p2.x, y := p1.y + p2.y }
+
+#eval addPoints { x := 1.5, y := 32 } { x := -8, y := 0.2 }
+--  { x := -6.500000, y := 32.200000 }
+
+def distance (p1 : Point) (p2 : Point) : Float :=
+   Float.sqrt (((p2.x - p1.x) ^ 2.0) + ((p2.y - p1.y) ^ 2.0))
+
+#eval distance { x := 1.0, y := 2.0 } { x := 5.0, y := -1.0 }
+--  5.000000
+
+--  Structures may share th same date components.
+
+structure Point3D where
+  x : Float
+  y : Float
+  z : Float
+deriving Repr
+
+def origin3D : Point3D := { x := 0.0, y := 0.0, z := 0.0 }
+
+--  Type must be specified
+--#check { x := 0.0, y := 0.0 }
+--  invalid {...} notation, expected type is not known
+
+#check ({ x := 0.0, y := 0.0 } : Point)  --  : Point
+
+
+--  1.4.1. Updating Structures
+
+--  Returns a new point.
+def zeroX' (p : Point) : Point :=
+  { x := 0, y := p.y }
+
+def zeroX (p : Point) : Point :=
+  { p with x := 0}
+
+--  Given a point ..
+
+def fourAndThree : Point :=
+  { x := 4.3, y := 3.4 }
+
+#eval fourAndThree
+--  { x := 4.300000, y := 3.400000 }
+
+#eval zeroX fourAndThree
+--  { x := 0.000000, y := 3.400000 }
+
+#eval fourAndThree
+--   x := 4.300000, y := 3.400000 } - original value
+
+
+--  1.4.2. Behind the Scenes
+
+--  constructor
+#check Point.mk 1.5 2.8
+--  { x := 1.5, y := 2.8 } : Point
+
+#check (Point.mk)
+--  Point -> Point -> Point
+
+--  accessor function
+
+#check (Point.x)
+--  Point.x : Point → Float
+
+#check (Point.y)
+--  Point.y : Point → Float
--- FunctionalProgramming/FunctionalProgramming/ch01/Exercises.lean
+++ FunctionalProgramming/FunctionalProgramming/ch01/Exercises.lean
@@ -32,3 +32,62 @@ def joinStringsWith (sep : String) (str1 : String) (str2 : String) :=
 
 def volume (height : Nat) (width : Nat) (depth : Nat) :=
   height * width * depth
+
+
+--  1.4.3. Exercises
+structure RectangularPrism where
+  height : Float
+  width  : Float
+  depth  : Float
+deriving Repr
+
+def volume' (prism : RectangularPrism) : Float :=
+  prism.height * prism.width * prism.depth
+
+
+structure Point where
+  x : Float
+  y : Float
+deriving Repr
+
+def distance (p1 : Point) (p2 : Point) : Float :=
+  Float.sqrt (((p2.x - p1.x) ^ 2.0) + ((p2.y - p1.y) ^ 2.0))
+
+structure Segment where
+  end1 : Point
+  end2 : Point
+deriving Repr
+
+def length  (seg : Segment) : Float :=
+  distance seg.end1 seg.end2
+
+def pt1 : Point := { x := 1.0, y := 2.0 }
+def pt2 : Point := { x := 5.0, y := -1.0 }
+
+def segment1 : Segment := { end1 := pt1, end2 := pt2 }
+
+#eval length segment1
+--  5.0000
+
+-- names introduced by RectangularPrism.
+#check RectangularPrism.height
+#check RectangularPrism.width
+#check RectangularPrism.depth
+
+--  What are tne names and type introduced by these structures?
+structure Hamster where
+  name : String
+  fluffy : Bool
+
+structure Book where
+  makeBook ::
+  title : String
+  author : String
+  price : Float
+
+#check Hamster.name    --  : String
+#check Hamster.fluffy  --  : Bool
+
+#check Book.title   --  : String
+#check Book.author  --  : String
+#check Book.price   --  : Float
